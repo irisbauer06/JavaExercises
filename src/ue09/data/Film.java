7ffc16fa0a6292eaeb589ce6f61e1a6fb76459f1
Iris Bauer <irisamelie.bauer@gmail.com>
2024-06-18 11:44:12 +0200

    ue09

---
+++ src/ue09/data/Film.java
@@ -0,0 +1,15 @@
+package ue09.data;
+
+/**
+ *Datenerhaltungklasse zur Repräsentation eines Films
+ * auf einem digitalem Medium
+ * @author iris06
+ */
+public class Film
+{
+  //Datenelemente
+  private final int id;
+  private final String titel, hauptdarsteller;
+  private final Genre genre;
+  private final int erscheinungsjahr;
+}
---
+++ src/ue09/data/Genre.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ue09.data;
+
+/**
+ *
+ * @author iris06
+ */
+public enum Genre
+{
+  
+}

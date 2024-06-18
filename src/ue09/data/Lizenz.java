06486348e7bd41aca0b69b4798dbe2aeed10eb87
Iris Bauer <irisamelie.bauer@gmail.com>
2024-06-18 11:57:55 +0200

    ue09

---
+++ src/ue09/data/Lizenz.java
@@ -0,0 +1,10 @@
+package ue09.data;
+
+/**
+ *
+ * @author iris06
+ */
+public enum Lizenz
+{
+  GEKAUFT, GELIEHEN,PRIVATKOPIE, RAUPKOPIERT;
+}
---
+++ src/ue09/data/Qualität.java
@@ -0,0 +1,10 @@
+package ue09.data;
+
+/**
+ *
+ * @author iris06
+ */
+public enum Qualität
+{
+  SD, HD_READY, FULL_HD, UHD;
+}

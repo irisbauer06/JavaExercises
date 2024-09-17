fff2c954fe2d892aba932e3ce2b82472fbf38733
Iris Bauer <irisamelie.bauer@gmail.com>
2024-09-17 16:01:46 +0200

    ue09

--- src/ue09/gui/FilmTableModel.java
+++ src/ue09/gui/FilmTableModel.java
@@ -1,5 +1,6 @@
 package ue09.gui;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import ue09.data.Film;
@@ -40,14 +41,26 @@ public class FilmTableModel extends AbstractTableModel
   public Object getValueAt(int rowIndex, int columnIndex)
   {
     final Film film = filme.get(rowIndex); //"Richtigen" Film holen
+    return
     switch (columnIndex)
     {
-      case 0 -> { return film.getId(); }
-      case 1 -> { return film.getTitel(); }
-    }
+      case  0 -> film.getId(); 
+      case  1 -> film.getTitel(); 
+      case  2 -> film.getHauptdarsteller();
+      case  3 -> film.getGenre(); //TODO toString() überlagern
+      case  4 -> film.getErscheinungsjahr();
+      case  5 -> String.format("%d:%02d,",
+                  film.getDauerInMinuten()/60,
+                  film.getDauerInMinuten()%60);
+      case  6 -> film.getFsk();
+      case  7 -> film.getLizenz();
+      case  8 -> film.getQuality();
+      case  9 -> film.isGesehen() ? "Ja" : "Nein";
+      case 10 -> DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                        .format(film.getErworbenAm());
+      default -> "Should not see me ...";
+    };
     
-    //TODO
-    return "??";
   }
   
 

c7a9a3d701dac2c053a402489583be026cbbbfd6
Iris Bauer <irisamelie.bauer@gmail.com>
2024-10-16 08:01:45 +0200

    writer

---
+++ src/ue09/test/Streamtest.java
@@ -0,0 +1,32 @@
+package ue09.test;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+/**
+ *
+ * @author iris06
+ */
+public class Streamtest
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+   
+      final Writer writer = new OutputStreamWriter(new FileOutputStream
+  ("C:\\Users\\iris06\\OneDrive\\Dokumente\\HTL_Kaindorf\\5. Jahrgang\\Informatik\\ABC.txt"), "cp1252");
+      writer.write("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+      writer.write("Das ist de zweite Zeile\n");
+      writer.write("ÄÖÜäöüß€\n");
+      
+      writer.close();
+    }
+    catch (Exception ex)
+    {
+      ex.printStackTrace();
+    }
+  }
+}

a61c91215076ccc4fe1d4142f13a3ed3bb234edd
Iris Bauer <irisamelie.bauer@gmail.com>
2025-01-21 15:40:44 +0100

    uebungen

--- src/ue12/modbus/ReadRegisterTelegram.java
+++ src/ue12/modbus/ReadRegisterTelegram.java
@@ -1,5 +1,6 @@
 package ue12.modbus;
 
+import com.fazecast.jSerialComm.SerialPort;
 import java.io.Serial;
 import ue12.serial.SimpleSerial;
 
@@ -9,6 +10,7 @@ import ue12.serial.SimpleSerial;
  */
 public class ReadRegisterTelegram extends AbstractModbusTelegram
 {
+  private int startAd;
   public ReadRegisterTelegram(
     SimpleSerial serial, int deviceAd, boolean useHoldingRegs,
     int startAd, int quantityRegs, int lengthOfAnswer)
@@ -16,6 +18,7 @@ public class ReadRegisterTelegram extends AbstractModbusTelegram
     super(
     serial, deviceAd, useHoldingRegs ? 3 : 4,
     calcXmtData(startAd, quantityRegs), 5 + quantityRegs*2);
+    this.startAd = startAd;
   }
    private static byte[] calcXmtData(int startAd, int quantityRegs)
    {
@@ -26,11 +29,30 @@ public class ReadRegisterTelegram extends AbstractModbusTelegram
      buffer[3] = getLoByte(quantityRegs);
      return buffer;
    }
+   
+   public int getRegister(int adr)
+   {
+     final byte[] rcvBuf = getRcvData();
+     final int index = (adr-startAd)*2;      //TODO: PLAUSI
+     final int
+      loByte = rcvBuf[index],
+      hiByte = rcvBuf[index+1];
+    return (hiByte<<8) | loByte;
+   }
+   
    public static void main(String[] args)
   {
-    try
+    try (final SimpleSerial serial = new SimpleSerial("ttyUSB0"))
     {
+      serial.open();
+      serial.setComPortParameters(57600, 8,
+        SerialPort.TWO_STOP_BITS, SerialPort.NO_PARITY);
+      serial.setComPortTimeouts(5_000);
       
+      final ReadRegisterTelegram telegram =
+        new ReadRegisterTelegram(serial, 2, false, 0x30, 4);
+      telegram.send();
+      telegram.receive();
     }
     catch (Exception ex)
     {
